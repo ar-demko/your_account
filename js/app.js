18e73ab96bfcf0e92554de9f594bd4935ef5e3f7
ar-demko <ag.roza.demko@gmail.com>
2017-06-13 13:50:29 +0200

    start js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,24 @@
-$(function(){
+var validateForm = (function() {
 
-});
+  //prywatne wlasciwosci
+  var options = {};
+
+  //metoda publiczna
+  var init = function(_options) {
+    //do naszego modulu bedziemy przekazywac opcje
+    //przekazane ustawimy w zmiennej options naszego modulu, lub ustawimy domyslne
+    options = {
+      form : _options.form || null,
+      classError : _options.classError || 'error'
+    }
+    if (options.form == null || options.form == undefined || options.form.length==0) {
+      console.warn('validateForm: Źle przekazany formularz');
+      return false;
+    }
+  }
+
+  return {
+    init : init
+  }
+
+})();
